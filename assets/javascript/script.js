314100bf550d8cab1650c0cabd53ee28389c7018
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-05-05 15:24:32 -0300

    finalização

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,56 @@
-let notas = [];
 
-while(true){
 
-    let acertos = 0
+const gabarito = ['B', 'D', 'A', 'A', 'E', 'C', 'E', 'A', 'B', 'B'];
 
-    for (let i = 0; i <= 10; i++){
-        
+const listnot = [];
+
+
+function respostas(nq) {
+    const resposta = prompt(`Digite a resposta da questão ${nq} (A, B, C, D ou E):`);
+    return resposta.toUpperCase();
+}
+
+let nmax = 0;
+let nmin = 10;
+let somatotal = 0;
+let parar = false
+
+
+while (!parar) {
+    const nome = prompt("Digite o nome do aluno (ou 'sair' para encerrar):");
+    if (nome === 'sair') {
+        parar = true;
+    }else{
+        let nota = 0;
+
+    for (let i = 1; i <= 10; i++) {
+        const resposta = respostas(i);
+        if (resposta === gabarito[i - 1]) {
+            nota++;
+        }
     }
 
+    listnot.push({ nome: nome, nota: nota });
+
+    if (nota > nmax) {
+        nmax = nota;
+    }
+    if (nota < nmin) {
+        nmin = nota;
+    }
+
+    somatotal += nota;
 }
+}
+
+for (let i = 0; i < + listnot.length; i++) {
+    console.log(`${listnot[i].nome}: ${listnot[i].nota}`);
+}
+
+const quantidadeAlunos = listnot.length;
+const mediaNotas = somatotal / quantidadeAlunos;
+
+console.log(`Quantidade de alunos: ${quantidadeAlunos}`);
+console.log(`Nota máxima: ${nmax}`);
+console.log(`Nota mínima: ${nmin}`);
+console.log(`Média das notas: ${mediaNotas}`);
