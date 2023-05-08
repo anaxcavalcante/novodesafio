b656afaae87b27b88239f0f3ba069afb8deefa40
vilasboasfabio <vilasboasfabio564@gmail.com>
2023-05-08 07:58:59 -0300

    ajuste

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,43 +13,37 @@ while (!parar) {
     const nome = prompt("Digite o nome do aluno (ou 'sair' para encerrar):");
     if (nome === 'sair') {
         parar = true;
-    }else{
+    } else {
         let nota = 0;
-    for (let i = 1; i <= 10; i++) {
-        const resposta = respostas(i);
-        if (resposta === gabarito[i - 1]) {
-            nota++;
+        for (let i = 1; i <= 10; i++) {
+            const resposta = respostas(i);
+            if (resposta === gabarito[i - 1]) {
+                nota++;
+            }
         }
-    }
-    listnot.push({ nome: nome, nota: nota });
+        listnot.push({ nome: nome, nota: nota });
 
-    if (nota > nmax) {
-        nmax = nota;
+        if (nota > nmax) {
+            nmax = nota;
+        }if (nota < nmin) {
+            nmin = nota;
+        }
+        somatotal += nota;
     }
-    if (nota < nmin) {
-        nmin = nota;
-    }
-    somatotal += nota;
 }
-}
+let alunoMaiorNota = listnot[0];
+let alunoMenorNota = listnot[0];
 for (let i = 0; i < + listnot.length; i++) {
     console.log(`${listnot[i].nome}: ${listnot[i].nota}`);
+    if (listnot[i].nota >= alunoMaiorNota.nota) {
+        alunoMaiorNota = listnot[i];
+    } if (listnot[i].nota <= alunoMenorNota.nota) {
+        alunoMenorNota = listnot[i];
+    }
 }
 const quantidadeAlunos = listnot.length;
 const mediaNotas = somatotal / quantidadeAlunos;
+console.log(`Aluno com a maior nota é ${alunoMaiorNota.nome} : ${alunoMaiorNota.nota}`);
+console.log(`Aluno com a menor nota é ${alunoMenorNota.nome} : ${alunoMenorNota.nota}`);
 console.log(`Quantidade de alunos: ${quantidadeAlunos}`)
 console.log(`Média das notas: ${mediaNotas}`);
-let alunoMaiorNota = listnot[0];
-for (let i = 1; i < listnot.length; i++) {
-    if (listnot[i].nota >= alunoMaiorNota.nota) {
-        alunoMaiorNota = listnot[i];
-      }
-    }
-    console.log(`Aluno com a maior nota: ${alunoMaiorNota.nome} : ${alunoMaiorNota.nota}`);
-    let alunoMenorNota = listnot[0];
-    for (let i = 1; i < listnot.length; i++) {
-        if (listnot[i].nota <= alunoMenorNota.nota) {
-            alunoMenorNota = listnot[i];
-          }
-        }
-        console.log(`Aluno com a menor nota: ${alunoMenorNota.nome} : ${alunoMenorNota.nota}`);
